ced04ed5fa0aa01a2609baa46d67179bd2c04a22
Zeros遇见 <zeros_zhag@163.cm>
2022-08-16 18:23:32 +0800

    001.监控数值变化

---
+++ 000.更新ReadMe文件/Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _000.更新ReadMe文件
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            StreamWriter _write = new StreamWriter(File.Open("./ReadMe.md", FileMode.OpenOrCreate));
+            _write.WriteLine("## 简介");
+            _write.WriteLine("该库用于一些`C#`语言的简单测试，每个项目为一个独立的模块，目录如下，可跳转对应的博客界面");
+            try
+            {
+                string[] _directories = null;
+                _directories = Directory.GetDirectories("./");
+                for (int i = 0; i < _directories.Length; i++)
+                {
+                    if (Path.GetFileName(_directories[i]).StartsWith("."))
+                    {
+                        continue;
+                    }
+                    _write.WriteLine(Path.GetFileName(_directories[i]));
+                }
+                MessageBox.Show("更新完毕...");
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("写入错误...");
+            }
+            finally
+            {
+                _write.Close();
+            }
+        }
+    }
+}
---
+++ 001.监控数值变化/Form1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _001.监控数值变化
+{
+    public partial class Form1 : Form
+    {
+        /// <summary>
+        /// 定义需要监测的数值
+        /// </summary>
+        private int _value = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            OnMyValueChanged += new Action(WhenValueChanged);
+            richTextBox1.AppendText($"初始_value = {mValue}\n");
+        }
+
+        private Action OnMyValueChanged;
+
+        /// <summary>
+        /// 使用属性的set来判断数值是否发生了改变
+        /// </summary>
+        public int mValue
+        {
+            get
+            {
+                return _value;
+            }
+            set
+            {
+                if (_value != value)
+                {
+                    _value = value;
+                    if (OnMyValueChanged != null)
+                    {
+                        OnMyValueChanged();
+                    }
+                }
+            }
+        }
+
+        private void WhenValueChanged()
+        {
+            richTextBox1.AppendText($"当前_value = {mValue}，");
+            richTextBox1.SelectionColor = Color.Red;
+            richTextBox1.AppendText($"_value发生了改变...\n");
+            richTextBox1.SelectionColor = Color.Black;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            mValue++;
+        }
+    }
+}
