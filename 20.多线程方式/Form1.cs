ae4d41133c9728d64cbe7e3d3bff1811a37d3b93
Zeros遇见 <zeros_zhag@163.cm>
2023-01-03 17:28:27 +0800

    20.多线程方式

---
+++ 20.多线程方式/Form1.cs
@@ -0,0 +1,205 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO.Ports;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _20.多线程方式
+{
+    public partial class Form1 : Form
+    {
+        private readonly HttpClient httpClient = new HttpClient();
+        private object _lock = new object();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button_sync_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Text = "";
+
+            var stopwatch = Stopwatch.StartNew();
+
+            foreach (var site in Contents.WebSites)
+            {
+                var result = DownloadWebSiteSync(site);
+                richTextBox1.Text += result;
+                Application.DoEvents();
+            }
+
+            richTextBox1.Text += $"总耗时: {stopwatch.Elapsed}{Environment.NewLine}";
+        }
+
+        private void ReportResult(string result)
+        {
+            Invoke(new Action(() =>
+            {
+                richTextBox1.Text += result;
+                Application.DoEvents();
+            }));
+        }
+
+        private async void button_async_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Text = "";
+
+            var stopwatch = Stopwatch.StartNew();
+            await DownLoad_Async();
+            richTextBox1.Text += $"总耗时: {stopwatch.Elapsed}{Environment.NewLine}";
+
+        }
+
+        public async Task DownLoad_Async()
+        {
+            // 每个网站一个下载线程
+            foreach (var site in Contents.WebSites)
+            {
+                byte[] result = await Task.Run(() =>
+                {
+                    return DownloadWebSite(site);
+                });
+                richTextBox1.Text += $"从{site}下载数据结束：共下载 {result.Length}字节。{Environment.NewLine}";
+            }
+        }
+
+        public byte[] DownloadWebSite(string url)
+        {
+            var response = httpClient.GetAsync(url).GetAwaiter().GetResult();
+            byte[] responsePayloadBytes = response.Content.ReadAsByteArrayAsync().GetAwaiter().GetResult();
+            return responsePayloadBytes;
+        }
+
+        private string DownloadWebSiteSync(string url)
+        {
+            var response = httpClient.GetAsync(url).GetAwaiter().GetResult();
+            var responsePayloadBytes = response.Content.ReadAsByteArrayAsync().GetAwaiter().GetResult();
+            return $"从{url}下载数据结束：共下载 {responsePayloadBytes.Length}字节。{Environment.NewLine}";
+        }
+
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Text = "";
+
+            var stopwatch = Stopwatch.StartNew();
+
+            Thread _thread = new Thread(() =>
+            {
+                foreach (var site in Contents.WebSites)
+                {
+                    var result = DownloadWebSiteSync(site);
+                    Invoke(new Action(() =>
+                    {
+                        richTextBox1.Text += result;
+                        Application.DoEvents();
+                    }));
+                }
+            });
+            _thread.Start();
+            // 如果这里使用了Join方法，会造成死锁,，且会阻塞UI线程
+            // _thread.Join();
+            richTextBox1.Text += $"总耗时: {stopwatch.Elapsed}{Environment.NewLine}";
+        }
+
+
+
+        /// <summary>
+        /// 异步并发
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void button2_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Text = "";
+
+            var stopwatch = Stopwatch.StartNew();
+
+            List<Task> DownLoadTaskList = new List<Task>();
+
+            foreach (var site in Contents.WebSites)
+            {
+                DownLoadTaskList.Add(Task.Run(() =>
+                {
+                    var response = httpClient.GetAsync(site).GetAwaiter().GetResult();
+                    var responsePayloadBytes = response.Content.ReadAsByteArrayAsync().GetAwaiter().GetResult();
+                    ReportResult($"从{site}下载数据结束：共下载 {responsePayloadBytes.Length}字节。{Environment.NewLine}");
+                }));
+            }
+            Task.WhenAll(DownLoadTaskList).ContinueWith(t =>
+            {
+                Invoke(new Action(() =>
+                {
+                    richTextBox1.Text += $"总耗时: {stopwatch.Elapsed}{Environment.NewLine}";
+                }));
+            });
+        }
+
+        private void button_download_async_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Text = "";
+
+            var stopwatch = Stopwatch.StartNew();
+
+            List<Task> DownLoadTaskList = new List<Task>();
+
+            foreach (var site in Contents.WebSites)
+            {
+                DownLoadTaskList.Add(DownLoad_Async(site));
+            }
+            Task.WhenAll(DownLoadTaskList).ContinueWith(t =>
+            {
+                Invoke(new Action(() =>
+                {
+                    richTextBox1.Text += $"总耗时: {stopwatch.Elapsed}{Environment.NewLine}";
+                }));
+            });
+        }
+
+
+        private async Task DownLoad_Async(string site)
+        {
+            var response = await httpClient.GetAsync(site);
+            var responsePayloadBytes = await response.Content.ReadAsByteArrayAsync();
+            ReportResult($"从{site}下载数据结束：共下载 {responsePayloadBytes.Length}字节。{Environment.NewLine}");
+        }
+    }
+
+
+    public class Contents
+    {
+        public static readonly IEnumerable<string> WebSites = new string[]
+        {
+            "https://www.zhihu.com",
+            "https://www.zhihu.com",
+            "https://www.zhihu.com",
+            "https://www.zhihu.com",
+            "https://www.zhihu.com",
+            "https://www.zhihu.com",
+            "https://www.zhihu.com",
+            "https://www.zhihu.com",
+            "https://www.zhihu.com",
+            "https://www.zhihu.com",
+            "https://www.zhihu.com",
+            "https://www.zhihu.com",
+            "https://www.zhihu.com",
+            "https://www.zhihu.com",
+            "https://www.zhihu.com",
+            "https://www.zhihu.com",
+            "https://www.zhihu.com",
+            "https://www.zhihu.com",
+            "https://www.zhihu.com",
+            "https://www.zhihu.com"
+        };
+    }
+}
---
+++ 20.多线程方式/Form1.designer.cs
@@ -0,0 +1,134 @@
+﻿namespace _20.多线程方式
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button_sync = new System.Windows.Forms.Button();
+            this.button_async = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button_sync
+            // 
+            this.button_sync.Location = new System.Drawing.Point(27, 18);
+            this.button_sync.Margin = new System.Windows.Forms.Padding(5);
+            this.button_sync.Name = "button_sync";
+            this.button_sync.Size = new System.Drawing.Size(120, 40);
+            this.button_sync.TabIndex = 0;
+            this.button_sync.Text = "同步";
+            this.button_sync.UseVisualStyleBackColor = true;
+            this.button_sync.Click += new System.EventHandler(this.button_sync_Click);
+            // 
+            // button_async
+            // 
+            this.button_async.Location = new System.Drawing.Point(27, 228);
+            this.button_async.Margin = new System.Windows.Forms.Padding(5);
+            this.button_async.Name = "button_async";
+            this.button_async.Size = new System.Drawing.Size(120, 40);
+            this.button_async.TabIndex = 1;
+            this.button_async.Text = "异步串行";
+            this.button_async.UseVisualStyleBackColor = true;
+            this.button_async.Click += new System.EventHandler(this.button_async_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(172, 18);
+            this.richTextBox1.Margin = new System.Windows.Forms.Padding(5);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(509, 596);
+            this.richTextBox1.TabIndex = 2;
+            this.richTextBox1.Text = "";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(27, 88);
+            this.button1.Margin = new System.Windows.Forms.Padding(5);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(120, 40);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "同步线程";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(27, 158);
+            this.button2.Margin = new System.Windows.Forms.Padding(5);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(120, 40);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "异步并发";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(27, 298);
+            this.button3.Margin = new System.Windows.Forms.Padding(5);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(120, 40);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "解决冷启动";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button_download_async_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 21F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(699, 628);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.button_async);
+            this.Controls.Add(this.button_sync);
+            this.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.Margin = new System.Windows.Forms.Padding(5);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button_sync;
+        private System.Windows.Forms.Button button_async;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
